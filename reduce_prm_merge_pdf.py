86eea6a8d8541fe999c8ccaf80e23eca9a3b7ad4
James Battat <jbattat@wellesley.edu>
2025-05-29 12:08:32 -0400

    tool to merge output pdfs

---
+++ reduce_prm_merge_pdf.py
@@ -0,0 +1,22 @@
+# FIXME: merge with LAr_PrM.py
+
+import os
+from glob import glob
+from PyPDF2 import PdfMerger
+
+pdf_dir = 'diagnostic_output'
+
+def pdf_merge():
+    ''' Merges all the pdf files in a given directory '''
+    merger = PdfMerger()
+    pdf_list = glob(os.path.join(pdf_dir, "2025*.pdf"))
+    pdf_list = sorted(pdf_list)
+    #[merger.append(pdf) for pdf in allpdfs]
+    [merger.append(pdf) for pdf in pdf_list]
+
+    out_pdf = os.path.join(pdf_dir, "diagnostic_all.pdf")
+    with open(out_pdf, "wb") as new_file:
+        merger.write(new_file)
+
+if __name__ == "__main__":
+    pdf_merge()
